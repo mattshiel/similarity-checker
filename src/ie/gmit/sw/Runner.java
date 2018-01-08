85c1b53e053ed64320d93b271284fc41123ed80b
Matthew Shiel <mattshiel@gmail.com>
2018-01-08 16:04:40 +0000

    Started with Runner and UI classes.

---
+++ src/ie/gmit/sw/Runner.java
@@ -0,0 +1,10 @@
+package ie.gmit.sw;
+
+public class Runner {
+	public static void main(String[] args) {
+		// Intstance of the user interaface
+		UI userInterface = new UI();
+		
+		// userInterface.run();
+	}
+}
